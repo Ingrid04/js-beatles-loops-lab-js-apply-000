2b5baa01c526f3a6535906f74a85d1798ad5bc9f
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-05-08 23:53:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // add solution here
+function theBeatlesPlay ( musicians, instruments) {
+  var sentences = [];
+  for (let counter = 0; counter < musicians.length; counter ++){
+    var sentence = musicians[counter] + ' plays ' + instruments[counter];
+    sentences.push(sentence);
+  }
+  return sentences;
+}
+var coolMusicians = ['Jlo', 'Anuel', 'Marc Anthony', 'Maluma'];
+var coolInstruments = ['Piano', 'Trumpet', 'Saxofon', 'Woodblock'];
+var beatleFacts = theBeatlesPlay(coolMusicians, coolInstruments);
+console.log(beatleFacts);
+
+function johnLennonFacts () {
+  const facts = ["He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"];
+}
