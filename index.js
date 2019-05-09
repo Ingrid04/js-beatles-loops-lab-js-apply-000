6cb697b8014eeba1218c9445a7b30946fd87bc63
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-05-09 01:06:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function johnLennonFacts (facts) {
   var crazyFacts =[];
   var counter = 0;
   while (counter < facts.length) {
-    crazyFacts.push(facts);
+    crazyFacts.push(repeat);
     counter ++ ;
   }
   return crazyFacts;
