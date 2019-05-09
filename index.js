8600a73c7af8c2600f19bab1e7d45d1c40d4495a
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-05-09 00:35:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ function theBeatlesPlay ( musicians, instruments) {
 // var beatleFacts = theBeatlesPlay(coolMusicians, coolInstruments);
 // console.log(beatleFacts);
 
-function johnLennonFacts () {
-  const facts = ["He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"];
+function johnLennonFacts (facts) {
 }
+const facts = ["He was the last Beatle to learn to drive!!!",
+  "He was never a vegetarian!!!",
+  "He was a choir boy and boy scout!!!",
+  "He hated the sound of his own voice!!!"];
